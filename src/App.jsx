08a3c938a8f0fcdf6b337e405b534888470a1f8e
mohit <mohitpardeshi729@gmail.com>
2023-01-17 12:59:19 +0530

    number generator using useState done , using click button

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,35 @@ import React from 'react'
 import Header from './components/Header'
 import Die from './components/Die'
 
-
-
-
 function App(){
+
+  function arrayGenerator(numbers,range){  //this function generated a random number array
+    const a = [];
+    for(let i  = 0 ; i<numbers ; i++){
+      const b = Math.floor(Math.random()*range)+1;
+      a.push(b);
+    }
+    return a;
+  }
+
+const [array , setArray]=React.useState(arrayGenerator(10,7))
+
+ function clickHandler(){
+    setArray(arrayGenerator(10,7))
+  }
+
+  const dieArray = array.map(item=>{
+    return <Die value={item} />
+  })
+
   return (
     <div className="App">
     <Header title="Play this game" description="Click the button to roll the dice"/>
       <div className="container">
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
+      {dieArray}
       </div>
       <div className="button-div">
-      <button className="button">Roll</button>
+      <button className="button" onClick={clickHandler}>Roll</button>
       </div>
     </div>
   )
