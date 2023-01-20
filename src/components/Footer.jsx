3e8a56406e99499fbb79ee2521f59c0d9a2d4e32
mohit <mohitpardeshi729@gmail.com>
2023-01-20 12:47:10 +0530

    code revied done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function(){
     return <div>
         <a href="https://github.com/TheMohit2003/ReactJS-number-game" target="_blank" className="footer_div">
-            <img src="../public/github.svg" className="githubIcon" alt="repo link"/>
+            <img src="/github.svg" className="githubIcon" alt="repo link"/>
         </a>
     </div>
 }
